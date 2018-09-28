a9c6b9415077b12be7330941376160feac56f085
GeorgeLijj <32992621+GeorgeLijj@users.noreply.github.com>
2018-09-28 21:21:53 +0800

    9.28

---
+++ Java/JavaCuontToNum/src/CountToNum.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+public class CountToNum {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		//byte[]b=new byte[100];
+		for(int i=0;i<5;i++) {
+			int num=System.in.read();
+			System.out.println((char)num);
+		}
+	}
+};
+/**
+ * ÊäÈë 
+ * 1k* M
+ * Êä³ö
+ * 1
+ * k
+ * *
+ *  
+ * M
+ */
+ 
